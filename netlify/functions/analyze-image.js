ec4e143f867cb0b60c345b53189dcbb6040f5a70
Darshan <77528189+Dqrshan@users.noreply.github.com>
2025-04-06 15:57:11 +0530

    update

--- netlify/functions/analyze-image.js
+++ netlify/functions/analyze-image.js
@@ -1,9 +1,8 @@
 exports.handler = async (event) => {
   try {
     const { prompt, imageBase64 } = JSON.parse(event.body);
-
+    console.log(prompt);
     const apiKey = process.env.VITE_GEMINI_API_KEY.replaceAll('"', '').replaceAll("'", '');
-    console.log(apiKey);
     const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${apiKey}`, {
       method: 'POST',
       headers: {
